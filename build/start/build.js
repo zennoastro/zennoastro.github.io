c9638a73a197d1f2922f166df995e8babeb6021b
Andrei Nikiforov <antiggo@amac.local>
2019-02-02 14:53:37 +0100

    logotype motion on mobile

--- build/start/build.js
+++ build/start/build.js
@@ -4350,6 +4350,14 @@ Beast.decl({
     },
 })
 Beast.decl({
+    Text: {
+        expand: function () {
+            this.domAttr('data-3200-start', 'color: rgba(255,255,255,1);' )
+            this.domAttr('data-3350-start', 'color: rgba(0,0,0,1);' )   
+        }
+    },
+})
+Beast.decl({
     Symbol: {
         inherits:['Grid'],
         expand: function () {
@@ -4422,14 +4430,6 @@ Beast.decl({
                 });                
               
             });
-        }
-    },
-})
-Beast.decl({
-    Text: {
-        expand: function () {
-            this.domAttr('data-3200-start', 'color: rgba(255,255,255,1);' )
-            this.domAttr('data-3350-start', 'color: rgba(0,0,0,1);' )   
         }
     },
 })
